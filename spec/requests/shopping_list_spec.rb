ecb03948f341fc785ebc6678e715e43e5651fbb6
limavfabio <92121463+limavfabio@users.noreply.github.com>
2023-05-29 16:33:46 -0300

    Spec for shopping list

--- spec/requests/shopping_list_spec.rb
+++ spec/requests/shopping_list_spec.rb
@@ -11,13 +11,26 @@ RSpec.describe 'ShoppingLists', type: :request do
   describe 'GET /index' do
     it 'returns the shopping list' do
       user = FactoryBot.create(:user)
-      recipes = FactoryBot.create_list(:recipe, 5, user:)
-      FactoryBot.create_list(:food, 5, recipe: recipes.sample, user:)
+
+      sign_in user
+
+      recipes = FactoryBot.create_list(:recipe, 5, user: user)
+      recipes.each do |recipe|
+        @foods = FactoryBot.create_list(:food, 5, recipe: recipe, user: user)
+      end
 
       get '/shopping_list'
 
-      expect(response).to have_http_status(:found)
+      expect(response).to have_http_status(:ok)
       expect(response.content_type).to include('text/html')
+
+      expect(response.body).to include('Definitely not')
+
+      @foods.each do |food|
+        expect(response.body).to include(food.name)
+        # expect(response.body).to include("#{food.quantity} #{food.measurement_unit}")
+        # expect(response.body).to include(food.price.to_s)
+      end
     end
   end
 end
